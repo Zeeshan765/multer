e5a7fd6930d2047e9200f9f6617d1abe89b490e6
Zeeshan Ashraf <zeeshanashraf765@gmail.com>
2022-03-09 11:56:27 +0500

    ok

--- client/src/pages/SingleHighBudget.jsx
+++ client/src/pages/SingleHighBudget.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './SingleHighBudget.css';
 import { addProduct } from '../redux/cartRedux';
 import { useDispatch } from 'react-redux';
-
+//<img src={data.picture} alt='meeeeee' />
+//width="100%"
+//style={{ height: "20rem" }}
 const SingleHighBudget = ({ data }) => {
   const dispatch = useDispatch();
   return (
     <>
       <div className='product-box'>
         <div className='upper-box'>
-          <img src={data.picture} alt='meeeeee' />
+          <img
+            src={'http://localhost:4000/api/products' + data.picture}
+            alt=''
+          />
         </div>
         <div className='lower-box'>
           <h3>{data.name}</h3>
